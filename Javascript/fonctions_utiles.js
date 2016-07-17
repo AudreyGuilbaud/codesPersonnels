5612fec150ebd5b201b46081629569a7741e6b32
AudreyGuilbaud <guilbaud.audrey@live.fr>
2016-07-17 17:34:05 +0200

    ajout fonctions js

--- Javascript/fonctions_utiles.js
+++ Javascript/fonctions_utiles.js
@@ -39,3 +39,62 @@ function nonModifiable() {
     $('.modif').attr('hidden', true);
     $('.nonmodif').prop('hidden', false);
 }
+
+
+//hidden et non hidden, possible aussi sur un document.ready, on click ou on change
+function afficherCacher()
+{
+    document.getElementById("id1").style.display = "block";
+    document.getElementById("id2").style.display = "none";
+}
+
+/**
+ * Simuler le clic sur un bouton en cliquant sur un autre
+ */
+
+$(document).ready(function () {
+    $('#bouton1').click(function () {
+        $("#bouton2").get(0).click();
+    });
+});
+
+
+//coche ou décoche toutes les cases du tableau
+$(document).ready(function () {
+    $('#checkallActiv').checkAll('#adherentActiv input:checkbox');  
+});
+// si les checkboxs sont cochées, coche la checkbox "tout" cocher et vice versa
+$(document).ready(function () {
+    $(".caseActiv").click(function () {
+        if ($(".caseActiv").length === $(".caseActiv:checked").length) {
+            $("#checkallActiv").prop("checked", true);
+        } else {
+            $("#checkallActiv").removeAttr("checked");
+        }
+    });
+});
+
+//diverses façons d'influer sur le html ou sur le php
+function diversControles() {
+    
+    //Changer la valeur d'un input
+    $('#id').val('Le matricule est obligatoire.');
+    
+    //Changer le css d'un champ
+    $('#id').css("border", "1px solid #ce352c");
+    
+    //Donner le focus à un input
+    $('#id').focus();
+    
+    //Ajouter une classe à un champ
+    $('#id').addClass("classe");
+    
+    //Retirer une classe à un champ
+    $('#id').removeClass("classe");
+    
+    //Simuler un click sur un lien
+    $('#idlien').get(0).click()
+    
+    //Envoyer un formulaire
+    $('#idformulaire').submit();
+}
